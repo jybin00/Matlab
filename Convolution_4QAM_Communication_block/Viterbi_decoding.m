56a4e8d14c59ff082224cda69367f1eb22baebe8
Yubeen Jo <jybin00@naver.com>
2023-01-05 12:29:15 +0900

    error 정정 중

--- Convolution_4QAM_Communication_block/Viterbi_decoding.m
+++ Convolution_4QAM_Communication_block/Viterbi_decoding.m
@@ -14,45 +14,69 @@ function decoded_output = Viterbi_decoding(demodulated_output, num_message_bit)
 %--------------------------------------------------------------------------------------
     Path_metric = inf(64, num_message_bit+7);              % 64 X 103 Path metric 저장 위한 matrix
     Message_bit = inf(64, num_message_bit + 7);           % path message array
+    Current_path = zeros(1, 6);
     for t = 1 : num_message_bit+6+1                             % tail bits 존재
-        codeword = demodulated_output(1, 2*t-1: 2*t);    % input을 두개씩 끊어서 codeword 구성
         if t == 1
             Path_metric(1, 1) = 0;                                        % 맨 처음 state
         elseif t > 1 && t < 6                                               % t = 2~6 까지는 state가 펼쳐지는 시간
+            codeword = demodulated_output(1, 1+2*(t-2): 2*(t-1));    % input을 두개씩 끊어서 codeword 구성
             for j = 1 : 2^(t-1)
-                Current_path = binVal(j, (1+(64/2^(t-1))*(j-1) ));          % ex) current_path   = [0 0 1 1 0 0]
+                Current_path = binVal(1+(64/2^(t-1))*(j-1), : );          % ex) current_path   = [0 0 1 1 0 0]
                 Prev_Path_zero = polyval([Current_path(2:6), 0], 2)+1;  % prev_path_from 0 = [0 1 1 0 0 0] 끝자리 0
                 Prev_Path_one  = polyval([Current_path(2:6), 1], 2)+1;  % prev_path_from 1 = [0 1 1 0 0 1] 끝자리 1
 
                 % BM = min{ 끝자리 0에서 온 PM + BM, 끝자리 1에서 온 PM + BM}
-                BM_zero = polyval( mod(output_zero(Prev_Path_zero) - codeword, 2), 2);
-                BM_one  = polyval( mod(output_one(Prev_Path_one) - codeword, 2), 2);
-                [Path_metric(j, t), Message_bit(j,t)] = min([Path_metric(Prev_Path_zero) + BM_zero, ...
-                    Path_metric(Prev_Path_one) + BM_one]);
+                if Current_path(1) == 0  % 입력 0을 받아서 현재 값이 되었는가?
+                    BM_zero = polyval( mod(output_zero(Prev_Path_zero) - codeword, 2), 2);
+                    BM_one  = polyval( mod(output_zero(Prev_Path_one) - codeword, 2), 2);
+                    [Path_metric((1+(64/2^(t-1))*(j-1)), t), Message_bit((1+(64/2^(t-1))*(j-1)),t)] = min([Path_metric(Prev_Path_zero, t-1) + BM_zero, ...
+                        Path_metric(Prev_Path_one, t-1) + BM_one]);
+                else % 입력 1을 받아서 현재 값이 되었는가?
+                    BM_zero = polyval( mod(output_one(Prev_Path_zero) - codeword, 2), 2);
+                    BM_one  = polyval( mod(output_one(Prev_Path_one) - codeword, 2), 2);
+                    [Path_metric((1+(64/2^(t-1))*(j-1)), t), Message_bit((1+(64/2^(t-1))*(j-1)),t)] = min([Path_metric(Prev_Path_zero, t-1) + BM_zero, ...
+                        Path_metric(Prev_Path_one, t-1) + BM_one]);
+                end
             end
-        elseif t > 97 && t < 103                                                        % 마지막 t = 98~103는 state가 접히는 시간
+        elseif t > 97                                                                          % 마지막 t = 98~103는 state가 접히는 시간
+            codeword = demodulated_output(1, 1+2*(t-2): 2*(t-1));    % input을 두개씩 끊어서 codeword 구성
             for j = 1 : 2^(103-t)
-                Current_path = binVal(j, (1+(64/2^(103-t))*(j-1) ));      % ex) current_path   = [0 0 1 1 0 0]
+                Current_path = binVal(1+(64/2^(t-1))*(j-1), : );            % ex) current_path   = [0 0 1 1 0 0]
                 Prev_Path_zero = polyval([Current_path(2:6), 0], 2)+1;  % prev_path_from 0 = [0 1 1 0 0 0] 끝자리 0
                 Prev_Path_one  = polyval([Current_path(2:6), 1], 2)+1;  % prev_path_from 1 = [0 1 1 0 0 1] 끝자리 1
 
                 % BM = min{ 끝자리 0에서 온 PM + BM, 끝자리 1에서 온 PM + BM}
-                BM_zero = polyval( mod(output_zero(Prev_Path_zero) - codeword, 2), 2);
-                BM_one  = polyval( mod(output_one(Prev_Path_one) - codeword, 2), 2);
-                [Path_metric(j, t), Message_bit(j,t)] = min([Path_metric(Prev_Path_zero) + BM_zero, ...
-                    Path_metric(Prev_Path_one) + BM_one]);
+                if Current_path(1) == 0  % 입력 0을 받아서 현재 값이 되었는가?
+                    BM_zero = polyval( mod(output_zero(Prev_Path_zero) - codeword, 2), 2);
+                    BM_one  = polyval( mod(output_zero(Prev_Path_one) - codeword, 2), 2);
+                    [Path_metric(1+(64/2^(t-1))*(j-1), t), Message_bit((1+(64/2^(t-1))*(j-1)),t)] = min([Path_metric(Prev_Path_zero, t-1) + BM_zero, ...
+                        Path_metric(Prev_Path_one, t-1) + BM_one]);
+                else % 입력 1을 받아서 현재 값이 되었는가?
+                    BM_zero = polyval( mod(output_one(Prev_Path_zero) - codeword, 2), 2);
+                    BM_one  = polyval( mod(output_one(Prev_Path_one) - codeword, 2), 2);
+                    [Path_metric(1+(64/2^(t-1))*(j-1), t), Message_bit((1+(64/2^(t-1))*(j-1)),t)] = min([Path_metric(Prev_Path_zero, t-1) + BM_zero, ...
+                        Path_metric(Prev_Path_one, t-1) + BM_one]);
+                end
             end
         else
+            codeword = demodulated_output(1, 1+2*(t-2): 2*(t-1));    % input을 두개씩 끊어서 codeword 구성
             for j = 1 : 64
                 Current_path = binVal(j,:);                                             % ex) current_path   = [0 0 1 1 0 0]
                 Prev_Path_zero = polyval([Current_path(2:6), 0], 2)+1;  % prev_path_from 0 = [0 1 1 0 0 0] 끝자리 0
                 Prev_Path_one  = polyval([Current_path(2:6), 1], 2)+1;  % prev_path_from 1 = [0 1 1 0 0 1] 끝자리 1
 
                 % BM = min{ 끝자리 0에서 온 PM + BM, 끝자리 1에서 온 PM + BM}
-                BM_zero = polyval( mod(output_zero(Prev_Path_zero) - codeword, 2), 2);
-                BM_one  = polyval( mod(output_one(Prev_Path_one) - codeword, 2), 2);
-                [Path_metric(j, t), Message_bit(j,t)] = min([Path_metric(Prev_Path_zero) + BM_zero, ...
-                    Path_metric(Prev_Path_one) + BM_one]);
+                if Current_path(1) == 0  % 입력 0을 받아서 현재 값이 되었는가?
+                    BM_zero = polyval( mod(output_zero(Prev_Path_zero) - codeword, 2), 2);
+                    BM_one  = polyval( mod(output_zero(Prev_Path_one) - codeword, 2), 2);
+                    [Path_metric(j, t), Message_bit((1+(64/2^(t-1))*(j-1)),t)] = min([Path_metric(Prev_Path_zero, t-1) + BM_zero, ...
+                        Path_metric(Prev_Path_one, t-1) + BM_one]);
+                else % 입력 1을 받아서 현재 값이 되었는가?
+                    BM_zero = polyval( mod(output_one(Prev_Path_zero) - codeword, 2), 2);
+                    BM_one  = polyval( mod(output_one(Prev_Path_one) - codeword, 2), 2);
+                    [Path_metric(j, t), Message_bit((1+(64/2^(t-1))*(j-1)),t)] = min([Path_metric(Prev_Path_zero, t-1) + BM_zero, ...
+                        Path_metric(Prev_Path_one, t-1) + BM_one]);
+                end
             end
         end
     end
