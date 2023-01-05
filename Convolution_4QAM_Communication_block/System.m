7054384a86c37c2356aa755333a62a70605e8ff5
Yubeen Jo <jybin00@naver.com>
2023-01-05 16:56:28 +0900

    input seqence 임의로 하나 만들어서 해보길 잘 했다.

--- Convolution_4QAM_Communication_block/System.m
+++ Convolution_4QAM_Communication_block/System.m
@@ -5,6 +5,13 @@ close
 input = randsrc(1, 96, [0 1]);
 %input =   [0  1  0  0  0  1  0  0  1  1  1  0  0  1  1  1  0  0  0  1  1  1  0  0  0  0 1 1 1 1  1 1  1  0  1  0  1  0  0  1 0  1 1  0  1 0  1  1  0  0  0  0 0 1 0 1 0  1 0 1 1 1 0 1 1 1 0 0 1 0 0 0  1 1 0 1 1 1 1 1 1 0 1 0 1 1 0 1 0 0  0  1 1 0  1  1]
 encoded_input = Convolution_code_tail_bit(input, 96);
-decoding = Viterbi_decoding(encoded_input, 96);
+for i = 1: length(encoded_input)/4
+    modulated_output(1, 1+2*(i-1):2*i) = Sixteen_QAM(encoded_input(1, 1+4*(i-1):4*i), 100);
+end
+Channel_output = AWGN_channel(modulated_output, 2);
+for i = 1:length(Channel_output)/2
+    demodulated_output(1, 1+4*(i-1):4*i) = ML_demodulation(Channel_output(1,1+2*(i-1):2*i), 100);
+end
+decoding = Viterbi_decoding(demodulated_output, 96);
 
 nnz(input-decoding)
