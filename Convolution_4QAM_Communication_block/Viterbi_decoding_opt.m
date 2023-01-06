db5ce63645c6fe388f48fef39cd29809508a8bb8
Yubeen Jo <jybin00@naver.com>
2023-01-06 11:20:04 +0900

    tab vs space

--- Convolution_4QAM_Communication_block/Viterbi_decoding_opt.m
+++ Convolution_4QAM_Communication_block/Viterbi_decoding_opt.m
@@ -50,7 +50,7 @@ function decoded_output = Viterbi_decoding_opt(demodulated_output, num_message_b
                     Message_bit((1+(64/2^(t-1))*(j-1)), t) = 1;
                 end
             end
-        elseif t > 97																			 % 마지막 t = 98~103는 state가 접히는 시간
+        elseif t > 97                                                                           % 마지막 t = 98~103는 state가 접히는 시간
             codeword = demodulated_output(1, 1+2*(t-2): 2*(t-1));    % input을 두개씩 끊어서 codeword 구성
             for j = 1 : 2^(103-t)
                 Current_path = j-1;                                                       % ex) current_path   = [0 0 1 1 0 0]
