20b724580c5c4ca21e7e247df806194addc7333c
Yubeen Jo <jybin00@naver.com>
2023-01-06 15:06:43 +0900

    주석 추가

--- Hamming_2PAM_Communication_block/Soft_decision_DEC.m
+++ Hamming_2PAM_Communication_block/Soft_decision_DEC.m
@@ -22,9 +22,9 @@ function decoded_ouptut = Soft_decision_DEC(received_signal)
     Error_Sqr = (code - received_signal).^2;  % 이렇게 계산하면 모든 행에서 값이 빠짐.
     for i = 1:16
         for j = 1:7
-            ML_metric(i, 1) = ML_metric(i, 1) + Error_Sqr(i, j);
+            ML_metric(i, 1) = ML_metric(i, 1) + Error_Sqr(i, j);   % ML_metric 계산. 코드에서 받은 값 빼서 제곱.
         end
     end
-    [~, min_distance] = min(ML_metric);
-    decoded_ouptut = code(min_distance, [3 5 6 7]);
+    [~, min_distance] = min(ML_metric);     % 최소가 되는 index 찾기.
+    decoded_ouptut = code(min_distance, [3 5 6 7]);   % 찾은 index로 decoding하기.
 end
