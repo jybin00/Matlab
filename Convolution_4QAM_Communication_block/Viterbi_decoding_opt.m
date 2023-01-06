426b07dedace29a870e905f404305d60572238f0
Yubeen Jo <jybin00@naver.com>
2023-01-06 11:35:31 +0900

    .

--- Convolution_4QAM_Communication_block/Viterbi_decoding_opt.m
+++ Convolution_4QAM_Communication_block/Viterbi_decoding_opt.m
@@ -27,7 +27,7 @@ function decoded_output = Viterbi_decoding_opt(demodulated_output, num_message_b
     Message_bit = inf(64, num_message_bit + 7);                            % path message array
     for t = 1 : num_message_bit+6+1                                              % tail bits 존재
         if t == 1
-            Path_metric(1, 1) = 0;                                                         % 맨 처음 state
+            Path_metric(1, 1) = 0;														  % 맨 처음 state
         elseif t > 1 && t <= 6                                                             % t = 2~6 까지는 state가 펼쳐지는 시간
             codeword = demodulated_output(1, 1+2*(t-2): 2*(t-1));    % input을 두개씩 끊어서 codeword 구성
             for j = 1 : 2^(t-1)
@@ -36,13 +36,13 @@ function decoded_output = Viterbi_decoding_opt(demodulated_output, num_message_b
                 Prev_Path_one  = mod(2*Current_path + 1, 64) +1;       % prev_path_from 1 = [0 1 1 0 0 1] 끝자리 1
 
                 % BM = min{ 끝자리 0에서 온 PM + BM, 끝자리 1에서 온 PM + BM}
-                if Current_path < 32                                                                               % 입력 0을 받아서 현재 값이 되었는가?
+                if Current_path < 32													 % 입력 0을 받아서 현재 값이 되었는가?
                     BM_zero = nnz( mod(output_zero(Prev_Path_zero, :) - codeword, 2)); % 끝자리가 0인 코드에 0이 들어왔을 때 outputd으로 BM 계산
                     BM_one  = nnz( mod(output_zero(Prev_Path_one, :) - codeword, 2));  % 끝자리가 1인 코드에 0이 들어왔을 때
                     Path_metric((1+(64/2^(t-1))*(j-1)), t) = min(Path_metric(Prev_Path_zero, t-1) + BM_zero, ...
                         Path_metric(Prev_Path_one, t-1) + BM_one);
                     Message_bit((1+(64/2^(t-1))*(j-1)),t) = 0;
-				else																					% 입력 1을 받아서 현재 값이 되었는가?
+				else																				% 입력 1을 받아서 현재 값이 되었는가?
                     BM_zero = nnz( mod(output_one(Prev_Path_zero, :) - codeword, 2));
                     BM_one  = nnz( mod(output_one(Prev_Path_one, :) - codeword, 2));
                     Path_metric((1+(64/2^(t-1))*(j-1)), t) = min(Path_metric(Prev_Path_one, t-1) + BM_one, ...
@@ -50,7 +50,7 @@ function decoded_output = Viterbi_decoding_opt(demodulated_output, num_message_b
                     Message_bit((1+(64/2^(t-1))*(j-1)), t) = 1;
                 end
             end
-        elseif t > 97																			% 마지막 t = 98~103는 state가 접히는 시간
+        elseif t > 97	  % 마지막 t = 98~103는 state가 접히는 시간
             codeword = demodulated_output(1, 1+2*(t-2): 2*(t-1));   % input을 두개씩 끊어서 codeword 구성
             for j = 1 : 2^(103-t)			
                 Current_path = j-1;                                                       % ex) current_path   = [0 0 1 1 0 0]
