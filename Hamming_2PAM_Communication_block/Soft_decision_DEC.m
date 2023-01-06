f469f1588bf3baad464b22f29b75a6e78956d249
Yubeen Jo <jybin00@naver.com>
2023-01-06 14:40:22 +0900

    hamming code r

--- Hamming_2PAM_Communication_block/Soft_decision_DEC.m
+++ Hamming_2PAM_Communication_block/Soft_decision_DEC.m
@@ -1,5 +1,20 @@
 %% Haming code soft decision decoding
 
 function decoded_ouptut = Soft_decision_DEC(received_signal)
-    
+    code = [0	0	0	0	0	0	0;
+                1	1	0	1	0	0	1;
+                0	1	0	1	0	1	0;
+                1	0	0	0	0	1	1;
+                1	0	0	1	1	0	0;
+                0	1	0	0	1	0	1;
+                1	1	0	0	1	1	0;
+                0	0	0	1	1	1	1;
+                1	1	1	0	0	0	0;
+                0	0	1	1	0	0	1;
+                1	0	1	1	0	1	0;
+                0	1	1	0	0	1	1;
+                0	1	1	1	1	0	0;
+                1	0	1	0	1	0	1;
+                0	0	1	0	1	1	0;
+                1	1	1	1	1	1	1;
 end
