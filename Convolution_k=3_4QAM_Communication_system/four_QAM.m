a0f3c8e7338f9a1ae295f018b773d98d8e5c0831
Yubeen Jo <jybin00@naver.com>
2023-01-09 15:48:54 +0900

    demodulator 완성

--- Convolution_k=3_4QAM_Communication_system/four_QAM.m
+++ Convolution_k=3_4QAM_Communication_system/four_QAM.m
@@ -1,43 +1,21 @@
 % 4QAM 
+% Grey coding 적용
+% dmin = 2*sqrt(Ebav/2)
 
-function modulated_signal = Sixteen_QAM(coded_signal, Esav)
+function modulated_signal = four_QAM(coded_signal, Ebav)
     switch_int = 0;
-    for i = 1 : 4
+    for i = 1 : 2
         coded_signal(i);
-        switch_int = switch_int + 2^(4-i)*coded_signal(i);
+        switch_int = switch_int + 2^(2-i)*coded_signal(i);
         switch switch_int               % grey coding
-            case 0                          % [0 0 0 0]
-                modulated_signal = [sqrt(Esav/10) sqrt(Esav/10)];
-            case 1                          % [0 0 0 1]
-                modulated_signal = [sqrt(Esav/10) 3*sqrt(Esav/10)];
-            case 2                          % [0 0 1 0]
-                modulated_signal = [3*sqrt(Esav/10) sqrt(Esav/10)];
-            case 3                          % [0 0 1 1]
-                modulated_signal = [3*sqrt(Esav/10) 3*sqrt(Esav/10)];
-            case 4                          % [0 1 0 0]
-                modulated_signal = [sqrt(Esav/10) -sqrt(Esav/10)];
-            case 5                          % [0 1 0 1]
-                modulated_signal = [sqrt(Esav/10) -3*sqrt(Esav/10)];
-            case 6                          % [0 1 1 0]
-                modulated_signal = [3*sqrt(Esav/10) -sqrt(Esav/10)];
-            case 7                          % [0 1 1 1]
-                modulated_signal = [3*sqrt(Esav/10) -3*sqrt(Esav/10)];
-            case 8                          % [1 0 0 0]
-                modulated_signal = [-sqrt(Esav/10) sqrt(Esav/10)];
-            case 9                          % [1 0 0 1]
-                modulated_signal = [-sqrt(Esav/10) 3*sqrt(Esav/10)];
-            case 10                        % [1 0 1 0]
-                modulated_signal = [-3*sqrt(Esav/10) sqrt(Esav/10)];
-            case 11                         % [1 0 1 1]
-                modulated_signal = [-3*sqrt(Esav/10) 3*sqrt(Esav/10)];
-            case 12                         % [1 1 0 0]
-                modulated_signal = [-sqrt(Esav/10) -sqrt(Esav/10)];
-            case 13                         % [1 1 0 1]
-                modulated_signal = [-sqrt(Esav/10) -3*sqrt(Esav/10)];
-            case 14                         % [1 1 1 0]
-                modulated_signal = [-3*sqrt(Esav/10) -sqrt(Esav/10)];
-            case 15                         % [1 1 1 1]
-                modulated_signal = [-3*sqrt(Esav/10) -3*sqrt(Esav/10)];
+            case 0                          % [0 0]
+                modulated_signal = [sqrt(Ebav/2) sqrt(Ebav/2)];
+            case 1                          % [0 1]
+                modulated_signal = [-sqrt(Ebav/2) sqrt(Ebav/2)];
+            case 2                          % [1 0]
+                modulated_signal = [sqrt(Ebav/2) -sqrt(Ebav/2)];
+            case 3                          % [1 1]
+                modulated_signal = [-sqrt(Ebav/2) -sqrt(Ebav/2)];
         end
     end
 end
