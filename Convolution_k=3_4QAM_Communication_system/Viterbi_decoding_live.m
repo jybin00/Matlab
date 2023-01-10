96aaa6f65ea2854fa5c249f8389e2592efa5af99
Yubeen Jo <jybin00@naver.com>
2023-01-10 14:39:24 +0900

    comment modified

--- Convolution_k=3_4QAM_Communication_system/Viterbi_decoding_live.m
+++ Convolution_k=3_4QAM_Communication_system/Viterbi_decoding_live.m
@@ -9,12 +9,12 @@ function decoded_output = Viterbi_decoding(demodulated_output, num_message_bit)
     output_one =  [1 1;  0 0;	0 1;	1 0];  % 1이 입력으로 들어왔을 때 p1, p2
                         	
 %--------------------------------------------------------------------------------------
-    Path_metric = inf(4, num_message_bit+2 + 1);                               % 4 X num_message_bit + 2 + 1 Path metric 저장 위한 matrix
+    Path_metric = inf(4, num_message_bit+2 + 1);                               % 64 X num_message_bit + 6 + 1 Path metric 저장 위한 matrix
     Message_bit = inf(4, num_message_bit + 2 + 1);                            % path message array
     for t = 1 : num_message_bit+2+1                                              % tail bits 존재
         if t == 1
             Path_metric(1, 1) = 0;	                                                      % 맨 처음 state
-        elseif t > 1 && t <= 2                                                             % t = 2는 state가 펼쳐지는 시간
+        elseif t > 1 && t <= 2                                                             % t = 2~2 까지는 state가 펼쳐지는 시간
             codeword = demodulated_output(1, 1+2*(t-2): 2*(t-1));    % input을 두개씩 끊어서 codeword 구성
             for j = 1 : 2^(t-1)
                 Current_path = (4/2^(t-1))*(j-1);                                  % ex) current_path   = [0 0], [1 0]
@@ -39,9 +39,9 @@ function decoded_output = Viterbi_decoding(demodulated_output, num_message_bit)
         elseif t > num_message_bit + 1	       % 마지막 2 state는 접히는 시간
             codeword = demodulated_output(1, 1+2*(t-2): 2*(t-1));   % input을 두개씩 끊어서 codeword 구성
             for j = 1 : 2^(num_message_bit + 2 + 1 -t)			
-                Current_path = j-1;                                                      % ex) current_path   = [0 0]
-                Prev_Path_zero = mod(2*Current_path, 4)+1;             % prev_path_from 0 = [0 0] 끝자리 0
-                Prev_Path_one  = mod(2*Current_path + 1, 4) +1;      % prev_path_from 1 = [0 1] 끝자리 1
+                Current_path = j-1;                                                      % ex) current_path   = [0 0 1 1 0 0]
+                Prev_Path_zero = mod(2*Current_path, 4)+1;             % prev_path_from 0 = [0 1 1 0 0 0] 끝자리 0
+                Prev_Path_one  = mod(2*Current_path + 1, 4) +1;      % prev_path_from 1 = [0 1 1 0 0 1] 끝자리 1
 
                 % BM = min{ 끝자리 0에서 온 PM + BM, 끝자리 1에서 온 PM + BM}
                 if Current_path < 32                                                    % 입력 0을 받아서 현재 값이 되었는가?
@@ -61,9 +61,9 @@ function decoded_output = Viterbi_decoding(demodulated_output, num_message_bit)
         else
             codeword = demodulated_output(1, 1+2*(t-2): 2*(t-1));    % input을 두개씩 끊어서 codeword 구성
             for j = 1 : 4
-                Current_path = j-1;                                                        % ex) current_path   = [0 0]
-                Prev_Path_zero = mod(2*Current_path, 4)+1;               % prev_path_from 0 = [0 0] 끝자리 0
-                Prev_Path_one  = mod(2*Current_path + 1, 4) +1;        % prev_path_from 1 = [0 1] 끝자리 1
+                Current_path = j-1;                                                        % ex) current_path   = [0 0 1 1 0 0]
+                Prev_Path_zero = mod(2*Current_path, 4)+1;               % prev_path_from 0 = [0 1 1 0 0 0] 끝자리 0
+                Prev_Path_one  = mod(2*Current_path + 1, 4) +1;        % prev_path_from 1 = [0 1 1 0 0 1] 끝자리 1
 
                 % BM = min{ 끝자리 0에서 온 PM + BM, 끝자리 1에서 온 PM + BM}
                 if Current_path < 2    % 입력 0을 받아서 현재 값이 되었는가?
