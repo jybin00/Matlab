b68819675182ef1484c3b2ee937f3bdb7e318a53
Yubeen Jo <jybin00@naver.com>
2023-01-11 13:48:12 +0900

    test code from matlab homepage

---
+++ Convolution_k=3_4QAM_Communication_system/test.m
@@ -0,0 +1,78 @@
+rng default
+M = 4;                % Modulation order
+k = log2(M);           % Bits per symbol
+EbNoVec = (4:10)';     % Eb/No values (dB)
+numSymPerFrame = 50; % Number of QAM symbols per frame
+
+berEstSoft = zeros(size(EbNoVec)); 
+berEstHard = zeros(size(EbNoVec));
+
+% trellis = poly2trellis(7,[171 133]);
+trellis = poly2trellis(3,[7 5]);
+tbl = 32;
+rate = 1/2;
+
+for n = 1:length(EbNoVec)
+    % Convert Eb/No to SNR
+    snrdB = EbNoVec(n) + 10*log10(k*rate);
+    % Noise variance calculation for unity average signal power
+    noiseVar = 10.^(-snrdB/10);
+    % Reset the error and bit counters
+    [numErrsSoft,numErrsHard,numBits] = deal(0);
+    
+    while numErrsSoft < 100 && numBits < 1e7
+        % Generate binary data and convert to symbols
+        dataIn = randi([0 1],numSymPerFrame*k,1);
+        
+        % Convolutionally encode the data
+        dataEnc = convenc(dataIn,trellis);
+        
+        % QAM modulate
+        txSig = qammod(dataEnc,M, ...
+            InputType='bit', ...
+            UnitAveragePower=true);
+        
+        % Pass through AWGN channel
+        rxSig = awgn(txSig,snrdB,'measured');
+        
+        % Demodulate the noisy signal using hard decision (bit) and
+        % soft decision (approximate LLR) approaches.
+        rxDataHard = qamdemod(rxSig,M, ...
+            OutputType='bit', ...
+            UnitAveragePower=true);
+        rxDataSoft = qamdemod(rxSig,M, ...
+            OutputType='approxllr', ...
+            UnitAveragePower=true, ...
+            NoiseVariance=noiseVar);
+        
+        % Viterbi decode the demodulated data
+        dataHard = vitdec(rxDataHard,trellis,tbl,'cont','hard');
+        dataSoft = vitdec(rxDataSoft,trellis,tbl,'cont','unquant');
+        
+        % Calculate the number of bit errors in the frame. 
+        % Adjust for the decoding delay, which is equal to 
+        % the traceback depth.
+        numErrsInFrameHard = ...
+            biterr(dataIn(1:end-tbl),dataHard(tbl+1:end));
+        numErrsInFrameSoft = ...
+            biterr(dataIn(1:end-tbl),dataSoft(tbl+1:end));
+        
+        % Increment the error and bit counters
+        numErrsHard = numErrsHard + numErrsInFrameHard;
+        numErrsSoft = numErrsSoft + numErrsInFrameSoft;
+        numBits = numBits + numSymPerFrame*k;
+
+    end
+    
+    % Estimate the BER for both methods
+    berEstSoft(n) = numErrsSoft/numBits;
+    berEstHard(n) = numErrsHard/numBits;
+end
+
+semilogy(EbNoVec,[berEstSoft berEstHard],'-*')
+hold on
+semilogy(EbNoVec,berawgn(EbNoVec,'qam',M))
+legend('Soft','Hard','Uncoded','location','best')
+grid
+xlabel('Eb/No (dB)')
+ylabel('Bit Error Rate')
