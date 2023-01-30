f56ea7e22315625825b851e5dccfe58cdd288452
Yubeen Jo <jybin00@naver.com>
2023-01-30 10:57:26 +0900

    disp off

--- Hamming_2PAM_Communication_block/Multi_m_Multi_SNR_System.m
+++ Hamming_2PAM_Communication_block/Multi_m_Multi_SNR_System.m
@@ -3,7 +3,7 @@ clear      % variable clear
 close     % figure close
 tic
 
-N_frames = 5000000;                             % number of symbols
+N_frames = 500000;                             % number of symbols
 EbNo_db = (0:10)';                                        % bit energy [dB]
 b_error = zeros(length(EbNo_db), 1);                   % error bit array
 b_error_h = zeros(length(EbNo_db), 1);                   % error bit array
@@ -18,7 +18,7 @@ rate = 4/7;
 
 generated_sequnece = randsrc(N_frames, 4, [0 1]);         % #of frame X message bits
 
-parfor n = 1:length(EbNo_db)                                         % Eb of No range
+for n = 1:length(EbNo_db)                                         % Eb of No range
     snrdB = EbNo_db(n) + 10*log10(k*rate);
     noise_Var = 10.^(-snrdB/10);
     sigma = sqrt(noise_Var/2)
@@ -34,7 +34,7 @@ parfor n = 1:length(EbNo_db)                                         % Eb of No 
         demodulated_signal = Two_PAM_dem(received_signal);                                      % LLR demodulation
         estimation_h = Hamming_DEC(demodulated_signal);                                          % DEC
         %-----------------------------------------------------------------------------------
-        estimation_s = Hamming_DEC(Soft_decision_DEC(received_signal', 1));               % soft decision
+        estimation_s = Hamming_DEC((Soft_decision_DEC(received_signal', 1))');               % soft decision
 
         bit_error_s = nnz(input - estimation_s);
         bit_error_h = nnz(input - estimation_h);
