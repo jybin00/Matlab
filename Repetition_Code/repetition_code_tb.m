0314d16dcdbca9b010ad26d244b63dc512a711a8
Yubeen Jo <jybin00@naver.com>
2023-02-14 15:11:20 +0900

    repetition code의 이론적인 ber을 구했다. 내장 함수인 berawgn이 틀렸나? 왜 다른 것인지 더 알아보아야겠다.

--- Repetition_Code/repetition_code_tb.m
+++ Repetition_Code/repetition_code_tb.m
@@ -2,8 +2,9 @@
 clc
 clear
 close
+tic
 
-rng("default")
+%%rng("default")
 
 eb_No_db = (0:15)';
 rate = 1/3;               % 3 repetition
@@ -17,10 +18,12 @@ No = 10.^(-snr_db/10);
 q = zeros(length(No),1);
 
 parfor i = 1:length(eb_No_db)
+    received_signal = zeros(1,3);
+    sigma = sqrt(No(i)/2);
     code = 2*repmat(message, 1, 3) -1;   % bpsk modulation
     for j = 1:N_frame
         code_k = code(j,:);
-        received_signal = awgn(code_k, snr_db(i), 'measured');
+        received_signal = code_k + sigma*randn(1,3);
         if sum((received_signal - 1).^2) < sum((received_signal + 1).^2)
             decoding = 1;
             if decoding == code_k(1)
@@ -39,8 +42,10 @@ parfor i = 1:length(eb_No_db)
     end
 end
 ber = bit_error/N_frame
+toc
 %%
 close all
+tic
 parfor i = 1:length(No)
     disp(i)
     fun = @(x,y,z) (1/(pi*No(i)).^(3/2))*exp(-((x-1).^2 + (y-1).^2 + (z-1).^2)/No(i));
@@ -54,14 +59,15 @@ parfor i = 1:length(No)
     
     q(i,1) = integral3(fun, xmin, xmax, ymin, ymax, zmin, zmax, 'Method', 'iterated');
 end
+toc
 %%
-
+close all
 grid on
-semilogy(eb_No_db, 1-q, 'go-')
+semilogy(eb_No_db, 1-q, 'kx-')
 hold on
 semilogy(eb_No_db, ber, '-bo')
 hold on
-semilogy(eb_No_db, berawgn(eb_No_db, "psk", 2, 'diff'), 'r')
+semilogy(eb_No_db, berawgn(eb_No_db, "psk", 2, 'nondiff'), 'r')
 axis([0 13 10^-5 1])
 legend('Soft decision theoretical ber', 'Repetition code soft decision ber', 'BPSK ber')
 
