5a6334e419960fc162a3313103f694076ac34474
Yubeen Jo <jybin00@naver.com>
2023-02-14 13:45:19 +0900

    repetition code theoretical ber 구한 것 같은데 실험 값은 그에 못 미침.

--- Repetition_Code/repetition_code_tb.m
+++ Repetition_Code/repetition_code_tb.m
@@ -3,17 +3,20 @@ clc
 clear
 close
 
-eb_No_db = (0:14)';
+rng("default")
+
+eb_No_db = (0:15)';
 rate = 1/3;               % 3 repetition
 l = 1;                        % bpsk 
 snr_db = eb_No_db + 10*log10(rate*l);
 
-N_frame = 100000;
+N_frame = 1000000;
 message = randi([0 1], N_frame,1);
 bit_error = zeros(1,length(eb_No_db));
+No = 10.^(-snr_db/10);
+q = zeros(length(No),1);
 
-for i = 1:length(eb_No_db)
-    No = 10^(-snr_db(i)/10)
+parfor i = 1:length(eb_No_db)
     code = 2*repmat(message, 1, 3) -1;   % bpsk modulation
     for j = 1:N_frame
         code_k = code(j,:);
@@ -26,7 +29,7 @@ for i = 1:length(eb_No_db)
                 bit_error(1,i) = bit_error(1,i) + 1;
             end
         else
-           decoding = -1;
+            decoding = -1;
             if decoding == code_k(1)
                 continue;
             else
@@ -37,13 +40,30 @@ for i = 1:length(eb_No_db)
 end
 ber = bit_error/N_frame
 %%
+close all
+parfor i = 1:length(No)
+    disp(i)
+    fun = @(x,y,z) (1/(pi*No(i)).^(3/2))*exp(-((x-1).^2 + (y-1).^2 + (z-1).^2)/No(i));
+    
+    xmin = -inf;
+    xmax = inf;
+    ymin = -inf;
+    ymax = inf;
+    zmin = @(x,y) -y-x;
+    zmax = inf;
+    
+    q(i,1) = integral3(fun, xmin, xmax, ymin, ymax, zmin, zmax, 'Method', 'iterated');
+end
+%%
 
 grid on
+semilogy(eb_No_db, 1-q, 'go-')
+hold on
 semilogy(eb_No_db, ber, '-bo')
 hold on
 semilogy(eb_No_db, berawgn(eb_No_db, "psk", 2, 'diff'), 'r')
-axis([0 14 10^-5 1])
-legend('repetition code ber', 'bpsk ber')
+axis([0 13 10^-5 1])
+legend('Soft decision theoretical ber', 'Repetition code soft decision ber', 'BPSK ber')
 
-xlabel('ber')
-ylabel('Eb/No [dB]')
+ylabel('BER')
+xlabel('Eb/No [dB]')
