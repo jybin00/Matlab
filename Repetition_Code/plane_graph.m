9aa310855a6d8e56fbde796429a48887eaae76c9
Yubeen Jo <jybin00@naver.com>
2023-02-14 10:18:44 +0900

    radius sqrt(3) sphere integration

--- Repetition_Code/plane_graph.m
+++ Repetition_Code/plane_graph.m
@@ -16,3 +16,14 @@ hold on
 plot3(1, 1, 1, "o")
 hold on
 plot3(-1, -1, -1, "o")
+
+fun = @(x,y,z) 1/(2*pi)^(3/2)*exp(-((x).^2 + (y).^2 + (z).^2)/2);
+
+xmin = -sqrt(3);
+xmax = sqrt(3);
+ymin = @(x) -sqrt(3-x.^2);
+ymax = @(x) sqrt(3-x.^2);
+zmin = @(x,y) -sqrt(3-x.^2 - y.^2);
+zmax = @(x,y) sqrt(3-x.^2 - y.^2);
+
+q = integral3(fun, xmin, xmax, ymin, ymax, zmin, zmax, 'Method', 'tiled')
