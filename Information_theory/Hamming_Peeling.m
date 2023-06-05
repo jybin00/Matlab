059e4a41c205db242bb0db7bb97f19ded00ba16e
Yubeen Jo <jybin00@naver.com>
2023-06-05 10:23:50 +0900

    정보이론 과제 틀 잡기

---
+++ Information_theory/Hamming_Peeling.m
@@ -0,0 +1,38 @@
+% Hamming code % Peeling decoder
+% FER measure at BEC according to SNR
+
+
+% message generation
+
+% G matrix
+
+% decoding mode
+
+% H matrix
+
+
+% for loop
+
+
+% decoding
+
+
+% plot
+
+
+
+
+
+
+
+function decoding = Peeling(Frame)
+    
+end
+
+function decoding = Syndrome(Frame)
+
+end
+
+function decoding = ML_decoding(Frame)
+
+end
